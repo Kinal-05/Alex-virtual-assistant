2a968e4b65d9d95622965087b1550354c3665317
Kinal-05 <kinalmistry0509@gmail.com>
2025-02-13 23:20:12 +0530

    initial commit

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "snowman": "https://www.youtube.com/watch?v=gset79KMmt0&ab_channel=Sia",
+    "blue": "https://www.youtube.com/watch?v=IpFX2vq8HKw&ab_channel=yungkai",
+    "wishes": "https://www.youtube.com/watch?v=Gz38Yj09k3A&ab_channel=HasanRaheem",
+    "lover": "https://www.youtube.com/watch?v=-BjZmE2gtdo&ab_channel=TaylorSwiftVEVO"
+}
